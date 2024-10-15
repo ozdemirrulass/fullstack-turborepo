ab71514a03a18e08533e2db334e50b9ebf51be3b
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-15 08:13:16 +0300

    form validation zod & formstate

--- apps/web/app/auth/signup/page.tsx
+++ apps/web/app/auth/signup/page.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
+import SignupForm from './signupForm'
 
 const SignUpPage = () => {
     return (
         <div className='bg-white p-8 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center'>
             <h1 className='text-center text-2xl font-bold mb-4'>Sign Up Page</h1>
-
+            <SignupForm />
             <div className='flex justify between text-sm'>
                 <p>Already have an account?</p>
                 <Link className='underline' href="auth/signin">
---
+++ apps/web/lib/auth.ts
@@ -0,0 +1,37 @@
+"use server"
+
+import { redirect } from "next/navigation";
+import { BACKEND_URL } from "./constants";
+import { FormState, SignupFormSchema } from "./type";
+
+export async function signUp(state: FormState, formData: FormData): Promise<FormState> {
+    const validationFields = SignupFormSchema.safeParse({
+        name: formData.get("name"),
+        email: formData.get("email"),
+        password: formData.get("password")
+    })
+
+    if (!validationFields.success) {
+        return {
+            error: validationFields.error.flatten().fieldErrors,
+        }
+    }
+
+    const response = await fetch(`${BACKEND_URL}/auth/signup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(validationFields.data)
+    })
+
+    if (response.ok) {
+        redirect('/auth/signin')
+    }
+    else
+        return {
+            message: response.status === 409
+                ? "The user is already exists"
+                : response.statusText
+        }
+}
