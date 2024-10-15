cd75cd3fd6e8f0b3e7cddc0a4f24f8ef46878dd2
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-15 05:15:19 +0300

    create user dto

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
+  @Post("signup")
+  registerUser() {
+
+  }
 }
