c62c5b172ca1a9d9a5ac4d049e7d2c352fdcbdbe
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-15 22:41:21 +0300

    add local strategy for login

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
+import { verify } from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,14 @@ export class AuthService {
             throw new ConflictException("User already exists!")
         }
         return this.userService.create(createUserDto)
+    }
+
+    async validateLocalUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+        if (!user) throw new UnauthorizedException("User not found!");
+        const isPasswordMatched = verify(user.password, password);
+        if (!isPasswordMatched) throw new UnauthorizedException("Invalid Credentials!");
+
+        return { id: user.id, name: user.name }
     }
 }
