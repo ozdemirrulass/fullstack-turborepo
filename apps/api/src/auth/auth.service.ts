a0020836b3e5abc86bd30f6ed93f6c80ded6b9c2
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-15 05:18:57 +0300

    register user initial structure

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    registerUser(createUserDto: CreateUserDto) {
+        throw new Error('Method not implemented.');
+    }
+}
